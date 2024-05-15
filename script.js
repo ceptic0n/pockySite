dee4ee4d70c51346f3778177884f78d81c307516
ceptic0n <58377659+ceptic0n@users.noreply.github.com>
2024-05-14 22:58:05 -0700

    changed it into a bentoBox generator

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log("eek");
+
+const bento = document.querySelector(".bento");
+const bentoButton = document.querySelector(".generator");
+
+const vegetable = ["brocoli", "salted tomato", "kimchi"]
+const protein = ["Beef & onyon", "egg", "Katsu"]
+const dessert = ["kit kat", "muffin", "PANCAKE"]
+
+
+
+
+bentoButton.addEventListener("click", function(e){
+        e.preventDefault();
+        console.log("eek!!");
+        let todaysBento = generateBento(vegetable, protein, dessert);
+
+        
+        bento.innerText = todaysBento;
+})
+
+function generateBento(veg, protein, dessert){
+    const randomNumber = Math.floor(Math.random() * 3);
+    return `${veg[randomNumber]}, ${protein[randomNumber]}, ${dessert[randomNumber]} + rice`
+  }
