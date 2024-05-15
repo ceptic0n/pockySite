2b43c6ef8a3fe38bcc5ca72d54796b41825f98cd
ceptic0n <58377659+ceptic0n@users.noreply.github.com>
2024-05-14 23:06:27 -0700

    adjusted generateBento for arrays of any size

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ console.log("eek");
 const bento = document.querySelector(".bento");
 const bentoButton = document.querySelector(".generator");
 
-const vegetable = ["brocoli", "salted tomato", "kimchi"]
-const protein = ["Beef & onyon", "egg", "Katsu"]
-const dessert = ["kit kat", "muffin", "PANCAKE"]
+const vegetable = ["brocoli", "salted tomato", "kimchi", ]
+const protein = ["Beef & onyon", "egg", "Katsu", "Chicken Wings", "Spinach Tamagoyaki", "Panfried Scallops","Salmon", "tuna"]
+const dessert = ["kit kat", "muffin", "PANCAKE", "gummies", "churro"];
 
 
 
@@ -20,6 +20,13 @@ bentoButton.addEventListener("click", function(e){
 })
 
 function generateBento(veg, protein, dessert){
-    const randomNumber = Math.floor(Math.random() * 3);
-    return `${veg[randomNumber]}, ${protein[randomNumber]}, ${dessert[randomNumber]} + rice`
-  }
+    const vegIndex = randomIndex(veg);
+    const proteinIndex = randomIndex(protein);
+    const dessertIndex = randomIndex(dessert);
+    
+    return `${veg[vegIndex]}, ${protein[proteinIndex]}, ${dessert[dessertIndex]} + rice`;
+}
+
+function randomIndex(array){
+    return Math.floor(Math.random() * array.length);
+}
